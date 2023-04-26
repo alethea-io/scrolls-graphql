65c75e9a64a1d4b8fa95edcc6a494892d726bdda
Aleksandar Djuric <aleksandardjuric@alethea.io>
2023-04-26 01:27:50 -0400

    fixed bug with address ada handles fn

--- src/schema.ts
+++ src/schema.ts
@@ -208,7 +208,7 @@ const getTokensByAddress: (input: {
           tokens.push(token)
         break
       case "handle":
-        if(token.policy_id == 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a')
+        if(token.policy_id.slice(2) == 'f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a')
           tokens.push(token)
         break
       default:
